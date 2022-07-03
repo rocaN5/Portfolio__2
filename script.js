b982c936b54da3bc45026a617eb08df049f6c53f
rocaN5 <47889456+rocaN5@users.noreply.github.com>
2022-07-03 23:44:12 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const menuActive = document.getElementsByClassName('menu__button')[0];
+const menuPop = document.getElementsByClassName('menu__popup')[0];
+const menuExit = document.getElementsByClassName('menu__exit')[0];
+const bodyOverflow = document.getElementsByClassName('overflow__hider')[0];
+
+menuActive.addEventListener('click', () =>{
+    menuPop.classList.toggle('active');
+    bodyOverflow.classList.toggle('overflow__active');
+});
+
+menuExit.addEventListener('click', () =>{
+    menuPop.classList.toggle('active');
+    bodyOverflow.classList.toggle('overflow__active');
+});
+
 const prevIcon = '<img src="./img/resent__projects/left.svg">'
 const nextIcon = '<img src="./img/resent__projects/right.svg">'
 
@@ -33,3 +48,4 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
